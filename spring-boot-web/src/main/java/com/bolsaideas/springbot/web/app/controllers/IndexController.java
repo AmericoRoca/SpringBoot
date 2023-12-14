e1e949e9482080745cea6777e2c0d5073e74cea4
Américo <americochiclana@gmail.com>
2023-12-14 19:57:37 +0100

    creating the index

---
+++ spring-boot-web/src/main/java/com/bolsaideas/springbot/web/app/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.bolsaideas.springbot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	
+	@RequestMapping({"/index", "/", "/home"})
+	public String index() {
+		return "index";
+	}
+
+}

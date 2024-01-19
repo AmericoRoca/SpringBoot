174da5c88a63a83cc8b5952f0be1136f8b775d2c
Américo <americochiclana@gmail.com>
2024-01-19 19:56:21 +0100

    Creation of the Controllers and the views

---
+++ spring-boot-web/src/main/java/com/bolsaideas/springbot/web/app/models/Usuario.java
@@ -0,0 +1,25 @@
+package com.bolsaideas.springbot.web.app.models;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	
+	
+	//Getter and Setter
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	//End Getter and Setter
+	
+	
+}

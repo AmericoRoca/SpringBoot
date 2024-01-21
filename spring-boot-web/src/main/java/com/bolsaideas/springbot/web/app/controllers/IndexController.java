31d45848938aa2e5fe9517ef975044d75fca91b3
Américo <americochiclana@gmail.com>
2024-01-21 19:39:33 +0100

    @PathVariable

--- spring-boot-web/src/main/java/com/bolsaideas/springbot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsaideas/springbot/web/app/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.bolsaideas.springbot.web.app.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsaideas.springbot.web.app.models.Usuario;
@@ -38,15 +40,21 @@ public class IndexController {
 	@GetMapping("/listar")
 	public String listar(Model model) {
 		
-		List<Usuario> usuarios = new ArrayList<>();
-		
-		usuarios.add(new Usuario("Americo", "Chiclana", "achiclana@java.com"));
-		usuarios.add(new Usuario("Jesus", "Gomez", "jgomez@java.com"));
-		usuarios.add(new Usuario("Paco", "Liso", "pliso@java.com"));
+
 		
 		model.addAttribute("titulo", "Lista de usuarios");
-		model.addAttribute("usuarios", usuarios);
 		return "listado";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Americo", "Chiclana", "achiclana@java.com"),
+				new Usuario("Jesus", "Gomez", "jgomez@java.com"),
+				new Usuario("Paco", "Liso", "pliso@java.com"),
+				new Usuario("Rocky", "Balboa", "rbalboa@java.com"));
+		
+		return usuarios;
 	}
 	
 	
